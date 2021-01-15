3d7e1659ae0d18589409506bea4a69d8f8c93f69
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-15 23:06:48 +0800

    重构：调整按钮大小

--- src/pages/Blank.vue
+++ src/pages/Blank.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <q-banner rounded class="bg-grey-1">
-      <span class="text-h6">常用操作：</span>
+      <span>快捷：</span>
       <router-link to="/wenda/list" style="text-decoration: none">
-        <q-chip icon="list" size="lg" color="primary" text-color="white">问答列表</q-chip>
+        <q-chip icon="list" size="md" color="primary" text-color="white">问答列表</q-chip>
       </router-link>
       <router-link to="/wenda/new" style="text-decoration: none">
-        <q-chip icon="add" size="lg" color="primary" text-color="white">新建问答</q-chip>
+        <q-chip icon="add" size="md" color="primary" text-color="white">新建问答</q-chip>
       </router-link>
       <router-link to="/wenda/markets" style="text-decoration: none">
-        <q-chip icon="storefront" size="lg" color="primary" text-color="white">问答市场</q-chip>
+        <q-chip icon="storefront" size="md" color="primary" text-color="white">问答市场</q-chip>
       </router-link>
     </q-banner>
     <router-view/>
