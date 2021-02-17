1fdfb4dde19b6ea4bf2b0843ae842081f974555c
千橘 雫霞 <qianjunakasumi@gmail.com>
2021-02-17 15:40:47 +0800

    重构：IDEA 教我做事

--- src/router/index.js
+++ src/router/index.js
@@ -15,8 +15,11 @@ Vue.use(VueRouter)
  */
 
 export default function (/* { store, ssrContext } */) {
-  const Router = new VueRouter({
-    scrollBehavior: () => ({ x: 0, y: 0 }),
+  return new VueRouter({
+    scrollBehavior: () => ({
+      x: 0,
+      y: 0
+    }),
     routes,
 
     // Leave these as they are and change in quasar.conf.js instead!
@@ -25,6 +28,4 @@ export default function (/* { store, ssrContext } */) {
     mode: process.env.VUE_ROUTER_MODE,
     base: process.env.VUE_ROUTER_BASE
   })
-
-  return Router
 }
