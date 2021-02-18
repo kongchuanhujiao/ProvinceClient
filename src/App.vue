8943ae9562b74259bef08f5e042e3e8d9c780edf
千橘 雫霞 <qianjunakasumi@gmail.com>
2021-02-18 19:55:53 +0800

    重构：优化页面结构

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,68 @@
 <template>
-  <div id="q-app">
-    <router-view/>
-  </div>
+  <q-layout id="q-app" view="hHh lpr fff" class="layout flex bg-grey-2">
+    <q-header class="layout flex bg-white">
+      <q-toolbar class="limited">
+        <q-btn flat round class="text-grey-14" icon="menu" aria-label="菜单">
+          <q-menu>
+            <q-list>
+              <q-item clickable exact v-for="i in menu" :key="i.name" :to="i.to">
+                <q-item-section avatar>
+                  <q-avatar size="md" :icon="i.icon" :color="i.bg" text-color="white"/>
+                </q-item-section>
+                <q-item-section>{{ i.name }}</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
+
+        <q-toolbar-title class="text-black text-weight-bold">{{ title }}</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="limited bg-white">
+      <router-view @name="name"/>
+    </q-page-container>
+  </q-layout>
 </template>
+
+<style scoped>
+.layout {
+  flex-flow: column;
+  align-items: center;
+}
+
+.limited {
+  width: 100%;
+  max-width: 1333px;
+}
+</style>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      menu: [
+        {
+          name: '问答',
+          icon: 'mark_chat_read',
+          bg: 'green',
+          to: '/wenda/home'
+        },
+        {
+          name: '帮帮团',
+          icon: 'school',
+          bg: 'info',
+          to: '/bangbangtuan/home'
+        }
+      ],
+      title: '空中与传统互动教学AI'
+    }
+  },
+  methods: {
+    name (n) {
+      this.title = n
+    }
+  }
 }
 </script>
---
+++ src/EntryPoint.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  name: 'EntryPoint',
+  mounted () {
+    this.$emit('name', this.$route.name)
+    this.$router.beforeEach((to, from, next) => {
+      this.$emit('name', to.name)
+      next()
+    })
+  }
+}
+</script>
