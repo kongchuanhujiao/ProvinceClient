4324825d6495f5d3886cf4622eb82c36476dd6b8
千橘 雫霞 <qianjunakasumi@gmail.com>
2021-02-18 17:06:33 +0800

    重构：优化显示效果

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,59 +1,41 @@
 <template>
-  <q-layout
-    view="lHh Lpr lFf"
-    class="flex"
-    style="flex-flow: column; align-items: center"
-  >
-    <q-header
-      class="flex bg-white"
-      style="flex-flow: column; align-items: center"
-    >
-      <q-toolbar
-        class="text-primary"
-        style="max-width: 1333px"
-      >
-        <q-btn
-          flat dense round
-          icon="menu"
-          aria-label="菜单"
-        >
+  <q-layout view="hHh lpr fff" class="layout flex">
+    <q-header class="layout flex bg-white">
+      <q-toolbar class="limited">
+        <q-btn flat round class="text-grey-14" icon="menu" aria-label="菜单">
           <q-menu>
-            <q-list style="width: 150px">
-
-              <q-item
-                clickable exact
-                v-for="i in menu"
-                :key="i.name"
-                :to="i.to"
-              >
+            <q-list>
+              <q-item clickable exact v-for="i in menu" :key="i.name" :to="i.to">
                 <q-item-section avatar>
-                  <q-avatar
-                    :icon="i.icon"
-                    size="md"
-                    :color="i.bg"
-                    text-color="white"
-                  />
+                  <q-avatar size="md" :icon="i.icon" :color="i.bg" text-color="white"/>
                 </q-item-section>
                 <q-item-section>{{ i.name }}</q-item-section>
               </q-item>
-
             </q-list>
           </q-menu>
         </q-btn>
 
-        <q-toolbar-title
-          class="text-center text-black text-weight-bold"
-        >
-          {{ title }}
-        </q-toolbar-title>
+        <q-toolbar-title class="text-black text-weight-bold"> {{ title }}</q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-page-container style="max-width: 1333px;width: 100%;">
+    <q-page-container class="limited bg-white">
       <router-view/>
     </q-page-container>
   </q-layout>
 </template>
+
+<style scoped>
+.layout {
+  flex-flow: column;
+  align-items: center;
+}
+
+.limited {
+  width: 100%;
+  max-width: 1333px;
+}
+</style>
 
 <script>
 export default {
@@ -61,12 +43,6 @@ export default {
   data () {
     return {
       menu: [
-        {
-          name: '欢迎',
-          icon: 'home',
-          bg: 'teal',
-          to: '/'
-        },
         {
           name: '问答',
           icon: 'mark_chat_read',
@@ -78,12 +54,6 @@ export default {
           icon: 'school',
           bg: 'info',
           to: '/bangbangtuan/home'
-        },
-        {
-          name: '校园云',
-          icon: 'cloud_queue',
-          bg: 'light-blue',
-          to: '/xiaoyuanyun/home'
         }
       ],
       title: this.$route.name
