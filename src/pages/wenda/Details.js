7bd7383b11b1e1c5726835d4e2f7f23fba75c6a4
千橘 雫霞 <qianjunakasumi@gmail.com>
2021-02-19 17:10:35 +0800

    史诗：问答详情页面

---
+++ src/pages/wenda/Details.js
@@ -0,0 +1,257 @@
+export default {
+  name: 'Details',
+  data () {
+    return {
+      data: {
+        calculations: {
+          count: 0,
+          right: [],
+          wrong: [{
+            type: '',
+            member: []
+          }]
+        },
+        group_name: '',
+        members: [],
+        questions: [
+          {
+            key: '',
+            options: [],
+            question: [],
+            status: 0,
+            target: 0,
+            type: 0
+          }
+        ]
+      },
+      type: ['选择题', '填空题'],
+      abc: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+      status: 0,
+      toggle: [{
+        label: '预备作答',
+        value: 0
+      }, {
+        label: '允许作答',
+        value: 1
+      }, {
+        label: '停止作答',
+        value: 2
+      }],
+      page: '0',
+      proneWrong: '暂无数据',
+      chart: {},
+      websocket: undefined,
+      close: undefined
+    }
+  },
+  methods: {
+    load () {
+      this.$axios.get('/apis/wenda/questions?id=' + this.$route.params.id).then(res => {
+        if (res.data.status !== 0) {
+          console.error('获取问题失败')
+          return
+        }
+        this.data = res.data.data
+        this.status = res.data.data.questions[0].status
+
+        this.histogram(
+          0,
+          'data-right',
+          [{
+            type: '正确人数 ' + this.data.calculations.right.length,
+            value: this.data.calculations.right.length
+          }])
+        this.histogram(
+          1,
+          'data-wrong',
+          [{
+            type: '错误人数 ' + (this.data.calculations.count - this.data.calculations.right.length),
+            value: this.data.calculations.count - this.data.calculations.right.length
+          }])
+        this.histogram(2, 'data-pronewrong', this.wrongDigest())
+        this.update()
+      })
+    },
+
+    badgeColor (s) {
+      switch (s) {
+        case 0:
+          return 'green-13'
+        case 1:
+          return 'light-blue-12'
+        case 2:
+          return 'red-12'
+      }
+    },
+    badgeLabel (s) {
+      switch (s) {
+        case 0:
+          return '未开始'
+        case 1:
+          return '答题中'
+        case 2:
+          return '已结束'
+      }
+    },
+
+    switchStatus () {
+      this.$q.dialog({
+        title: '更改状态',
+        message: '确实更改状态为“' + this.badgeLabel(this.status) + '"吗？',
+        cancel: true
+      }).onOk(() => {
+        this.$axios.put(
+          '/apis/wenda/questions/status',
+          {
+            id: Number(this.$route.params.id),
+            status: this.status
+          }
+        ).then(res => {
+          if (res.data.status !== 0) {
+            this.status = this.data.questions[0].status
+            this.$q.notify({
+              message: '更改状态失败',
+              position: 'top-right'
+            })
+            return
+          }
+          this.data.questions[0].status = this.status
+          this.$q.notify({
+            message: '成功更改状态：' + this.badgeLabel(this.status),
+            position: 'top-right'
+          })
+        })
+      }).onCancel(() => {
+        this.status = this.data.questions[0].status
+      })
+    },
+    switchWebsocket () {
+      switch (this.data.questions[0].status) {
+        case 0:
+          try {
+            this.websocket.close()
+            clearInterval(this.close)
+          } catch {
+          }
+          break
+        case 1:
+          this.websocket = new WebSocket(this.address() + '/apis/wenda/runtime?id=' + this.$route.params.id)
+          this.websocket.onopen = () => {
+            this.close = setInterval(() => {
+              try {
+                this.websocket.send('keep heart')
+              } catch (e) {
+              }
+            }, 50000)
+          }
+          this.websocket.onmessage = msg => {
+            const data = JSON.parse(msg.data)
+            console.log('服务器推送问答数据：', data)
+            this.data.calculations = data.Calculation
+            this.update()
+          }
+          break
+        case 2:
+          try {
+            this.websocket.close()
+            clearInterval(this.close)
+          } catch (e) {
+          }
+          break
+      }
+    },
+
+    histogram (id, elm, data) {
+      const chart = new this.$g2.Chart({
+        container: elm,
+        autoFit: true,
+        width: 120,
+        height: 120
+      })
+
+      chart.data(data)
+      chart.scale('sales', { nice: true })
+      chart.interval().position('type*value')
+
+      chart.render()
+      this.chart[id] = chart
+    },
+    updateHistogram (id, data) {
+      this.chart[id].changeData(data)
+    },
+
+    rightRate () {
+      const r = this.data.calculations.right.length / this.data.calculations.count
+      if (isNaN(r)) {
+        return 0
+      }
+      return Math.floor(r * 100)
+    },
+    wrongRate () {
+      return 100 - this.rightRate()
+    },
+    wrongDigest () {
+      const d = this.data.calculations.wrong
+      if (d.length === 0) {
+        return [{
+          type: '暂无数据',
+          value: 0
+        }]
+      }
+      for (let i = 0; i < d.length; i++) {
+        const t = d[i]
+        d[i] = {
+          type: t.type,
+          value: t.member.length
+        }
+      }
+      d.sort((a, b) => {
+        return a.member.length - b.member.length
+      })
+      d.reverse()
+      return d
+    },
+
+    update () {
+      this.switchWebsocket()
+      this.proneWrong = this.wrongDigest()[0].type
+
+      this.updateHistogram(0, [{
+        type: '正确人数 ' + this.data.calculations.right.length,
+        value: this.data.calculations.right.length
+      }])
+      this.updateHistogram(1, [{
+        type: '错误人数 ' + (this.data.calculations.count - this.data.calculations.right.length),
+        value: this.data.calculations.count - this.data.calculations.right.length
+      }])
+      this.updateHistogram(2, this.wrongDigest())
+    },
+
+    praise () {
+      this.$axios.post('/apis/wenda/praise', { id: Number(this.$route.params.id) }).then(res => {
+        if (res.data.status !== 0) {
+          this.$q.notify({
+            message: '表扬失败',
+            position: 'top-right'
+          })
+          return
+        }
+        this.$q.notify({
+          message: '表扬成功',
+          position: 'top-right'
+        })
+      })
+    },
+
+    address () {
+      let p = 'wss:'
+      if (location.protocol === 'http:') {
+        p = 'ws:'
+      }
+      return p + '//' + location.host
+    }
+  },
+  mounted () {
+    this.load()
+  }
+}
---
+++ src/pages/wenda/Details.vue
@@ -0,0 +1,113 @@
+<template>
+  <q-page class="q-px-md">
+    <div class="q-my-sm">
+      <span class="text-h5">题目</span>
+      <span class="text-subtitle1 q-mx-xs">[{{ type[data.questions[0].type] }}]</span>
+      <q-badge outline color="grey-13" :label="data.group_name"/>
+    </div>
+
+    <div class="text-body1">
+      <div class="q-my-xs">
+        问题：
+        <span v-for="( item, i ) in data.questions[0].question" :key="i">
+              <span v-if="item.type === 'text'">{{ item.data }}</span>
+              <img v-else :src="'/assets/pictures/questions/' + item.data" class="question-img" alt="问题图片">
+        </span>
+      </div>
+      <div class="q-my-xs">
+        选项：
+        <q-badge outline class="q-ma-xs" color="light-blue-12"
+                 v-for="( option, i ) in data.questions[0].options"
+                 :key="i"
+                 :label="abc[i] + '. ' + option"
+        />
+      </div>
+      <div class="q-my-xs">答案：{{ data.questions[0].key }}</div>
+    </div>
+
+    <div class="q-my-md">
+      <span class="text-h6">状态</span>
+      <q-badge outline class="q-mx-sm" :satus1="(data.questions[0].status)"
+               :label="badgeLabel(data.questions[0].status)"
+               :color="badgeColor(data.questions[0].status)"
+      />
+      <q-badge outline :label="'已作答人数：' + data.calculations.count" color="blue"/>
+    </div>
+    <q-btn-toggle unelevated class="q-mb-md" color="grey-2" text-color="grey-9" toggle-color="light-blue-12"
+                  v-model="status"
+                  @input="switchStatus"
+                  :options="toggle"
+    />
+
+    <q-separator class="q-mb-md"/>
+
+    <div>
+      <span class="text-h6">正确</span>
+      <div class="flex justify-around">
+        <q-circular-progress show-value class="text-positive" size="100px" color="positive" :thickness="0.1"
+                             :value="rightRate()"
+        >
+          {{ rightRate() }}%
+        </q-circular-progress>
+
+        <div id="data-right"/>
+      </div>
+
+      <q-expansion-item switch-toggle-side class="q-mt-md">
+        <template slot="header">
+          <q-item-section>回答正确的学生</q-item-section>
+          <q-item-section side>
+            <q-btn size="sm" icon="thumb_up_off_alt" @click="praise">表扬他们</q-btn>
+          </q-item-section>
+        </template>
+
+        <q-separator/>
+
+        <q-list separator>
+          <q-item class="items-center" v-for="( stu, i ) in data.calculations.right" :key="i">
+            {{ data.members[stu] }}
+          </q-item>
+        </q-list>
+      </q-expansion-item>
+    </div>
+
+    <div class="q-my-md">
+      <span class="text-h6">错误</span>
+      <div class="flex justify-around">
+        <q-circular-progress show-value class="text-red" size="100px" color="red" :thickness="0.1"
+                             :value="wrongRate()"
+        >
+          {{ wrongRate() }}%
+        </q-circular-progress>
+
+        <div id="data-wrong"/>
+      </div>
+
+      <q-expansion-item switch-toggle-side class="q-mt-md" label="回答错误的学生">
+        <q-separator/>
+        <q-expansion-item switch-toggle-side
+                          v-for="( sdqdq, i ) in data.calculations.wrong"
+                          :key="i"
+                          :label="'错误类型：'+sdqdq.type"
+        >
+          <q-separator/>
+          <q-list separator>
+            <q-item class="items-center" v-for="( stu, ii ) in sdqdq.member" :key="ii">
+              {{ data.members[stu] }}
+            </q-item>
+          </q-list>
+        </q-expansion-item>
+      </q-expansion-item>
+    </div>
+
+    <span class="text-h6">易错选项：{{ proneWrong }}</span>
+    <div id="data-pronewrong"/>
+
+  </q-page>
+</template>
+
+<script src="./Details.js"/>
+
+<style scoped>
+
+</style>
