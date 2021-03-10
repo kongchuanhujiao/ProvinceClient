39728132772f7a144781c4ea2a3fd229f4da2607
千橘 雫霞 <i@qianjunakasumi.ren>
2021-03-10 22:41:44 +0800

    修复：因文档错误导致的请求失败

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import axios from 'axios'
 import { LocalStorage } from 'quasar'
 
-axios.defaults.headers.common['Authentication'] = 'Bearer ' + LocalStorage.getItem('token')
+axios.defaults.headers.common.Authorization = 'Bearer ' + LocalStorage.getItem('token')
 
 Vue.prototype.$axios = axios
