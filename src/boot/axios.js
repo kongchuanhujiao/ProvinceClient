184f76714a5a522a25b341f1df2fa329329767e6
千橘 雫霞 <i@qianjunakasumi.ren>
2021-03-13 18:03:24 +0800

    史诗：支持请求失败自动打印错误信息

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import axios from 'axios'
-import { LocalStorage } from 'quasar'
+import { LocalStorage, Notify } from 'quasar'
 
 axios.defaults.headers.common.Authorization = 'Bearer ' + LocalStorage.getItem('token')
+
+axios.interceptors.response.use(resp => {
+  if (resp.status === 200 && resp.data.status === 0) {
+    return resp
+  } else {
+    Notify.create({ message: '失败：' + resp.data.message, position: 'top-right' })
+    Promise.reject(resp)
+  }
+}, err => {
+  switch (err.response.status) {
+    case 401:
+      Notify.create({ message: '请重新登录', position: 'top-right' })
+      break
+    default:
+      Notify.create({ message: '请求服务器失败，错误码：' + err.response.status, position: 'top-right' })
+  }
+  Promise.reject(err)
+})
 
 Vue.prototype.$axios = axios
